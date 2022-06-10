4dd58af5fa8d527ee0bffbf2ec6e17693cfcc5d9
Pintoko Daruadi <pintoko.mpd@gmail.com>
2022-06-10 09:59:44 +0700

    move Resttemplate to quotecontroller

--- src/main/java/com/docdoc/learnspring/link2/QuoteController.java
+++ src/main/java/com/docdoc/learnspring/link2/QuoteController.java
@@ -1,11 +1,18 @@
 package com.docdoc.learnspring.link2;
 
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class QuoteController {
+
+    @Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder){
+		return builder.build();
+	}
 
     @GetMapping("/quote")
     public Quote quote(RestTemplate restTemplate){
