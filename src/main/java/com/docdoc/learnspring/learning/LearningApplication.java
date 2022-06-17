0f8d57f9191e45841cdcb0b502bce4342746cc15
Pintoko Daruadi <pintoko.mpd@gmail.com>
2022-06-17 15:40:19 +0700

    link6 JPA

--- src/main/java/com/docdoc/learnspring/learning/LearningApplication.java
+++ src/main/java/com/docdoc/learnspring/learning/LearningApplication.java
@@ -2,12 +2,14 @@ package com.docdoc.learnspring.learning;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
-@ComponentScan(basePackages = "com.docdoc.learnspring")
+@SpringBootApplication(scanBasePackages = {"com.docdoc.learnspring"})
 @ImportResource("classpath:/link4IntegratingData/integration.xml")
+@EnableJpaRepositories("com.docdoc.learnspring.link6")
+@EntityScan("com.docdoc.learnspring.link6")
 public class LearningApplication {
 
 	public static void main(String[] args) {
