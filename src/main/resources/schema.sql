17f5e8ebeef377735b4149dcc475cc5e5fa2714d
Pintoko Daruadi <pintoko.mpd@gmail.com>
2022-06-17 18:30:23 +0700

    link7 Accessing Relational Data using JDBC with Sp

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,2 +1,10 @@
 drop table BOOKINGS if exists;
 create table BOOKINGS(ID serial, FIRST_NAME varchar(5) NOT NULL);
+
+drop table CUSTOMERS if exists;
+create table CUSTOMERS(id serial, first_name varchar(255), last_name varchar(255));
+
+insert into CUSTOMERS(first_name, last_name) values ('John', 'Woo');
+insert into CUSTOMERS(first_name, last_name) values ('Jeff', 'Dean');
+insert into CUSTOMERS(first_name, last_name) values ('Josh', 'Bloch');
+insert into CUSTOMERS(first_name, last_name) values ('Josh', 'Long');
